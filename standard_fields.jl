968e862cd5445e6410089542603e9317e7d490a0
dilruu89 <diruu89@gmail.com>
2018-04-04 23:53:17 +0930

    standard fields
    Add new fields

--- standard_fields.jl
+++ standard_fields.jl
@@ -9,3 +9,10 @@ Fields[1]=FieldDefinition("Birth Day",1,DiscreteUniform(1,365))
 Fields[2]=FieldDefinition("Birth Year",1,DiscreteUniform(1,100))
 Fields[3]=FieldDefinition("Country of birth (Exclude Australia)",1,DiscreteUniform(1,365))
 Fields[4]=FieldDefinition("Sex",1,DiscreteUniform(1,2))
+
+eye_color_freq = [8, 55, 2, 1, 8, 26]
+Fields[5]=FieldDefinition("Eye Color",0,Categorical(eye_color_freq/sum(eye_color_freq)))
+Fields[6] = FieldDefinition("name", 2 , DiscreteUniform(1,1000))
+Fields[7] = FieldDefinition("address(meshblock)", 4 , DiscreteUniform(1,347627))
+Fields[8] = FieldDefinition("address(SA1)", 4 , DiscreteUniform(1,1000))
+Fields[9] = FieldDefinition("address", 0 , DiscreteUniform(1,1000))
